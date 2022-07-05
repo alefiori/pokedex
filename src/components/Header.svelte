6bff88dfa818963f443195e70df8b63436f43689
Alessandro Fiori <alefiori97@gmail.com>
2022-07-05 16:48:22 +0200

    project setup and add navigation

---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import logo from '../assets/pokemon.png'
+</script>
+
+<header>
+  <div class="logo">
+    <img src={logo} alt="pokemon" />
+  </div>
+</header>
---
+++ src/pages/List.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Link } from 'svelte-navigator'
+  import Card from '../components/Card.svelte'
+</script>
+
+<p>List Page</p>
+<Link to="detail">
+  <Card />
+</Link>
